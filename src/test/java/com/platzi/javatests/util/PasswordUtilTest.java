5ba587ea59de8262d0cc0022954279a40dbaa393
pcorqui <pcorqui@gmail.com>
2022-09-18 20:51:19 -0500

    first commit

---
+++ src/test/java/com/platzi/javatests/util/PasswordUtilTest.java
@@ -0,0 +1,29 @@
+package com.platzi.javatests.util;
+
+import org.junit.Test;
+
+import static com.platzi.javatests.util.PasswordUtil.SecurityLevel.*;
+import static org.junit.Assert.*;
+
+public class PasswordUtilTest {
+
+    @Test
+    public void weak_when_has_less_than_8_letters(){
+        assertEquals(WEAK, PasswordUtil.assessPassword("1234567"));
+    }
+
+    @Test
+    public void weak_when_has_less_than_only_letters(){
+        assertEquals(WEAK, PasswordUtil.assessPassword("1234567"));
+    }
+
+    @Test
+    public void medium_when_has_letters_and_numbers(){
+        assertEquals(MEDIUM, PasswordUtil.assessPassword("abcde123"));
+    }
+
+    @Test
+    public void medium_when_has_letters_and_numbers_and_simbols(){
+        assertEquals(STRONG, PasswordUtil.assessPassword("abcde123!"));
+    }
+}
---
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -0,0 +1,56 @@
+package com.platzi.javatests.util;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+//La clase debe ser public
+public class StringUtilTest {
+    //en los test no se utiliza main si no una clase llamada test
+    /*public static void main(String[] args) {
+
+        //creando test unitarios: aquellos que se encargan de probar solo una funcion o clase
+
+        //assertequal(StringUtil.repeat("Hola",3),"HolaHolaHola");
+
+        //assertequal(StringUtil.repeat("hola",1),"hola");
+
+        //utilizando jUnit
+        Assert.assertEquals("HolaHolaHola",StringUtil.repeat("Hola",3));
+        Assert.assertEquals("Hola",StringUtil.repeat("Hola",1));
+    }*/
+
+    public static void assertequal(String actual, String expectec){
+        if(!actual.equals(expectec)){
+            throw new RuntimeException(actual + "is not equal to expectec: " + expectec);
+        }
+    }
+
+    //cada test debe tener una sola comprobaion y se pueden correr independiente
+    @Test
+    public void testRepeat(){
+        Assert.assertEquals("HolaHolaHola",StringUtil.repeat("Hola",3));
+        Assert.assertEquals("Hola",StringUtil.repeat("Hola",1));
+    }
+
+    @Test
+    public void test_repeat_only_one_time(){
+        Assert.assertEquals("HolaH",StringUtil.repeat("Hola",1));
+    }
+
+    @Test
+    public void test_repeat_multiples_times(){
+        Assert.assertEquals("HolaHolaHola",StringUtil.repeat("Hola",3));
+    }
+
+    @Test
+    public void test_repeat_zero_times(){
+        Assert.assertEquals("",StringUtil.repeat("Hola",0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_repeat_negative_times(){
+        StringUtil.repeat("",-1);
+    }
+}
