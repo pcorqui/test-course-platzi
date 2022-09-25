d442947ea9343fa668ec492b69dc410dca3cf6fb
Paul Cortes <pcorqui@gmail.com>
2022-09-25 16:23:55 -0500

    practice TDD

---
+++ src/main/java/com/platzi/javatests/utill/DateUtil.java
@@ -0,0 +1,12 @@
+package com.platzi.javatests.utill;
+
+public class DateUtil {
+
+    public static boolean isLeapYear(int year){
+
+        if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            return true;
+
+        return false;
+    }
+}
---
+++ src/test/java/com/platzi/javatests/discount/PriceCalculatorTest.java
@@ -0,0 +1,37 @@
+package com.platzi.javatests.discount;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class PriceCalculatorTest {
+
+    @Test
+    public void total_zero_when_there_are_prices(){
+        PriceCalculator calculator = new PriceCalculator();
+        assertThat(calculator.getTotal(),is(0.0));
+    }
+
+    @Test
+    public void total_is_the_sum_of_prices() {
+        PriceCalculator priceCalculator = new PriceCalculator();
+
+        priceCalculator.addPrice(10.2);
+        priceCalculator.addPrice(15.5);
+
+        assertThat(priceCalculator.getTotal(), is(25.7));
+    }
+
+    @Test
+    public void apply_discount_to_prices() {
+        PriceCalculator priceCalculator = new PriceCalculator();
+
+        priceCalculator.addPrice(12.5);
+        priceCalculator.addPrice(17.5);
+
+        priceCalculator.setDiscount(50);
+
+        assertThat(priceCalculator.getTotal(), is(15.0));
+    }
+}
